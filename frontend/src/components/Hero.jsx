e79cec63654f3a8b542e49bcfc539df2e66456f2
YahyaLimbo <m.yahya@hotmail.it>
2025-08-10 11:31:43 +0200

    landing page work in progress

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,21 @@
+function Hero () {
+    return (
+        <section className = "hero">
+            <h1>Monitoring subscribtions is easier now!</h1>
+            <p>Staying on track on mulitple subscriptions, bills and membership can be really tiring. </p>
+            <p>SubTrack deliver a digital solution and it's easy and intuitive.</p>
+                
+            <section>
+                <p1 className="second-paragraph">
+                    this is an example of paragraph
+                </p1>
+                <img src="src/assets/sampleImage.jpg" alt="Bill management services image"/>
+                <onclick button className ="buttons">View plans</onclick>
+                <onclick button className ="buttons">Find out more</onclick>
+            </section>
+
+        
+        </section>
+    );
+};
+export default Hero;
--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,10 +1,11 @@
 import NavBar from "./NavBar";
 import "../LandingPage.css";
-
+import Hero from "./Hero";
 function LandingPage() {
     return (
         <div className="landing-page">
             <NavBar />
+            <Hero/>
         </div>
     )
 }
