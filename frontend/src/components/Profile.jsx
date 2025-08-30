0b858fdb235a65382682a0ce6f301bae3342f431
Samsku <turunensami54@gmail.com>
2025-08-30 12:50:27 +0300

    contrast improvements + profile styles

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,4 +1,5 @@
 import {getUser} from '../utils/auth';
+import '../UserPage.css';
 const Profile = () => {
     const user = getUser()
     return (
@@ -6,17 +7,17 @@ const Profile = () => {
         //style in css 
         <div className="profile-container">
             <ul className="profile-details">
-                <li><strong>Profile Name:</strong>
+                <li className="profile-item" style={{marginBottom: "0"}}><strong>Profile Name:</strong>
                     {user.name}</li>
 
                     <img></img>
-                    <li><strong>Email:</strong>{user.email}</li>
-                    <li><strong>Password:</strong></li>
-                    <li><strong>Phone number:</strong></li>
-                    <li><strong>Location:</strong></li>
-                    <li><strong>Account creation date:</strong></li>
-                    <li><strong>Last login:</strong></li>
-                    <li><strong>Subscription plan:</strong></li>
+                    <li className="profile-item"><strong>Email:</strong>{user.email}</li>
+                    <li className="profile-item"><strong>Password:</strong></li>
+                    <li className="profile-item"><strong>Phone number:</strong></li>
+                    <li className="profile-item"><strong>Location:</strong></li>
+                    <li className="profile-item"><strong>Account creation date:</strong></li>
+                    <li className="profile-item"><strong>Last login:</strong></li>
+                    <li className="profile-item"><strong>Subscription plan:</strong></li>
                                
 
             </ul>
